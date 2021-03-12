636e7824baf4de57085e27eec38834b3918db422
José María Beristain <josemaria_1998@outlook.com>
2021-03-12 17:07:48 -0600

    Final de serilización de datos
    con prueba de prints

---
+++ Proyecto5to/Assets/Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public SaveData SD;
+    public int HS;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SD.Cargar();
+        //SD.Data.HighScore = 0;
+        HS = SD.Data.HighScore;
+        print("high score:" + HS);
+    }
+
+    private void OnApplicationQuit()
+    {
+        SD.Guardar();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (HS > SD.Data.HighScore) {
+            print("nuevo high score:" + HS);
+            SD.Data.HighScore = HS;
+        }
+    }
+}
---
+++ Proyecto5to/Assets/Scripts/SaveData.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveData : MonoBehaviour
+{
+    public SAVEDATACLASS Data; //es visible desde Inspector
+
+    public void Guardar()
+    {
+        //Generamos el formato Json
+        string Archivo = JsonUtility.ToJson(Data);
+        //Lo guardamos con PlayerPrefs
+        PlayerPrefs.SetString("KeySave", Archivo);
+    }
+    public void Cargar()
+    {
+        string Archivo = PlayerPrefs.GetString("KeySave");
+        //Solo seguridad que si exista algo
+        if (!string.IsNullOrEmpty(Archivo))
+        {
+            Data = JsonUtility.FromJson<SAVEDATACLASS>(Archivo);
+        }
+    }
+}
+
+[System.Serializable]
+public class SAVEDATACLASS
+{
+    public int HighScore;
+}
