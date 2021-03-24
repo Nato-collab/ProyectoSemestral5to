12f7e7e794206e37b6df06861d61581c35356608
José María Beristain <dedocebabotas@hotmail.com>
2021-03-24 11:51:07 -0600

    simulación inicial

--- Proyecto5to/Assets/Scripts/emisor_liquido.cs
+++ Proyecto5to/Assets/Scripts/emisor_liquido.cs
@@ -6,27 +6,48 @@ public class emisor_liquido : MonoBehaviour
 {
     public GameObject gota;
     public GameObject instance;
+    public GameObject[] instances;
+    public int instances_index;
+    public float tiempo=0;
     public float frecuencia; //frecuencia en que se lanzan las gotas
     public float tamaño_gota;
-    public float radio_lanzamiento; //cuanto va avariar la pocición de lanzamiento de la gota
+    public float radio_lanzamiento;//cuanto va avariar la pocición de lanzamiento de la gota
+    public int max_gotas;//número máximo de gotas
+    public bool emitir;
 
     // Start is called before the first frame update
     void Start()
     {
+        instances = new GameObject[max_gotas];
+        emitir = true;
+        instances_index = 0;
         LanzarGota();
+        Invoke("detenerEmision", tiempo);
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void detenerEmision() {
+        emitir = false;
     }
 
     public void LanzarGota() {
         instance=Instantiate(gota);
-        instance.transform.position = new Vector3(Random.Range(-radio_lanzamiento,radio_lanzamiento),transform.position.y, Random.Range(-radio_lanzamiento, radio_lanzamiento));
+        instance.transform.position = new Vector3(Random.Range(-radio_lanzamiento,radio_lanzamiento)+transform.position.x,transform.position.y, Random.Range(-radio_lanzamiento, radio_lanzamiento)+transform.position.z);
         instance.transform.localScale = Vector3.one * tamaño_gota;
+        try
+        {
+            instances[instances_index] = instance;
+        }
+        catch
+        {
+            emitir = false;
+            print("máximo de particulas alcanzado");
+        }
+        instances_index++;
         CancelInvoke("LanzarGota");
-        Invoke("LanzarGota", frecuencia);
+        if (emitir)
+        {
+            Invoke("LanzarGota", frecuencia);
+        }
     }
 }
---
+++ Proyecto5to/Assets/Scripts/gota.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gota : MonoBehaviour
+{
+    Collider col;
+    // Start is called before the first frame update
+    void Start()
+    {
+        col = gameObject.GetComponent<Collider>();
+        col.enabled = false;
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.CompareTag("vaso")) {
+            col.enabled = true;
+        }
+    }
+}
