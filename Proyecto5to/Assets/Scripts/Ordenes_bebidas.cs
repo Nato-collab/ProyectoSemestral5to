976d8a7b6229f620ad6bc7928b3ca84b89535afe
José María Beristain <dedocebabotas@hotmail.com>
2021-04-12 19:20:14 -0500

    ordenes programadas
    se puede detectar si el vaso fué colocado para entregar y se da o resta puntos dependiendo de que tan bien se preparó la bebida

---
+++ Proyecto5to/Assets/Scripts/Ordenes_bebidas.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ordenes_bebidas : MonoBehaviour
+{
+    public vaso vas;
+    public GameManager GM;
+    public int penalty_bebida_vacia;
+    public bool recibir_bebida;
+    private float recive_time=1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        recibir_bebida = true;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.CompareTag("vaso")&&recibir_bebida) {
+            recibir_bebida = false;
+            vas = collision.gameObject.GetComponent<vaso>();
+            Invoke("bebidaCheck", recive_time);
+        }
+    }
+
+    private void bebidaCheck() {
+        if (vas.drink_name == "mata toros") {
+            if (vas.whiskey_cantidad > 70)
+            {
+                GM.score += 70 - (vas.whiskey_cantidad - 70);
+            }
+            else {
+                GM.score += 70 + (vas.whiskey_cantidad - 70);
+            }
+            if (vas.champagne_cantidad > 70)
+            {
+                GM.score += 70 - (vas.champagne_cantidad - 70);
+            }
+            else
+            {
+                GM.score += 70 + (vas.champagne_cantidad - 70);
+            }
+        }
+
+        if (vas.drink_name == "")
+        {
+            GM.score -= penalty_bebida_vacia;
+        }
+
+        vas.Reset_vaso();
+        recibir_bebida = true;
+    }
+}
--- Proyecto5to/Assets/Scripts/vaso.cs
+++ Proyecto5to/Assets/Scripts/vaso.cs
@@ -9,7 +9,14 @@ public class vaso : MonoBehaviour
     public int champagne_cantidad;
     public List<GameObject> whiskey;
     public List<GameObject> champagne;
+    public Vector3 initial_pos;
+
     // Start is called before the first frame update
+    private void Start()
+    {
+        drink_name="";
+        initial_pos = transform.position;
+    }
 
     private void Update()
     {
@@ -37,7 +44,7 @@ public class vaso : MonoBehaviour
         }
     }
 
-    private void OnCollisionExit(Collision collision)
+    /*private void OnCollisionExit(Collision collision)
     {
         //quitar los ingredientes a la lista que pertenecen cuando enstán fuera
         if (collision.gameObject.CompareTag("whis"))
@@ -48,12 +55,28 @@ public class vaso : MonoBehaviour
         {
             champagne.Remove(collision.gameObject);
         }
-    }
+    }*/
 
     //checar si se cumplen los requerimientos de ingredientes para hacer una bebida
     public void checkForDrinks() {
-        if (whiskey_cantidad > 60 && champagne_cantidad > 60) {
-            drink_name = "mata-toros";
+        if (whiskey_cantidad > 10 && champagne_cantidad > 10) {
+            drink_name = "mata toros";
         }
+    }
+
+    public void Reset_vaso() {
+        if (whiskey_cantidad > 0) {
+            for (int i = 0; i < whiskey.Count; i++) {
+                whiskey[i].GetComponent<gota>().pool_out();
+            }
+        }
+        if (champagne_cantidad > 0)
+        {
+            for (int i = 0; i < champagne.Count; i++)
+            {
+                champagne[i].GetComponent<gota>().pool_out();
+            }
+        }
+        transform.position = initial_pos;
     }
 }
